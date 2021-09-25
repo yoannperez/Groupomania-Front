0bc9300561a47330714e49826b1fb2e17858b1f8
Yoann Perez <yoann.perez@gmail.com>
2021-09-25 15:47:24 +0200

    Modifs début Part 2

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,44 +4,25 @@ import Card from "./Card";
 
 const Countries = () => {
   const [data, setData] = useState([]);
-  // eslint-disable-next-line
-  const [sortedData, setSortedData] = useState([]);
-  const [playOnce, setPlayOne] = useState(true);
   const [rangeValue, setRangeValue] = useState(40);
   const [selectedRadio, setSelectedRadio] = useState("");
   const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
 
   useEffect(() => {
-    if (playOnce) {
-      axios
-        .get("https://restcountries.com/v3/all")
-        // .get("https://restcountries.com/v3/all?fields=name,capital,flag,area, region")
-        .then((res) => {
-          setData(res.data);
-          setPlayOne(false);
-        });
-    }
-    const sortedCountry = () => {
-      const countryObj = Object.keys(data).map((i) => data[i]);
-      const sortedArray = countryObj.sort((a, b) => {
-        return b.area - a.area;
-      });
-      sortedArray.length = rangeValue;
-      setSortedData(sortedArray);
-    };
+    axios.get("https://restcountries.com/v3/all")
+    .then((res) => {
+      setData(res.data);
+    });
+  }, []);
 
-    sortedCountry();
-    // eslint-disable-next-line
-  }, [data, rangeValue, playOnce]);
-  // console.log(sortedData)
   return (
     <div className="countries">
       <div className="sort-container">
         <input type="range" min="1" max="250" value={rangeValue} onChange={(e) => setRangeValue(e.target.value)} />
         <ul>
-          {radios.map((radio, index) => {
+          {radios.map((radio) => {
             return (
-              <li key={`${radio}-${index}`}>
+              <li key={radio}>
                 <input type="radio" value={radio} id={radio} checked={radio === selectedRadio} onChange={(e) => setSelectedRadio(e.target.value)} />
                 <label htmlFor={radio}>{radio}</label>
               </li>
@@ -49,13 +30,16 @@ const Countries = () => {
           })}
         </ul>
       </div>
-      <div className="cancel">{selectedRadio && <h5 onClick ={()=> setSelectedRadio("")}>Annuler recherche</h5>}</div>
+      <div className="cancel">{selectedRadio && <h5 onClick={() => setSelectedRadio("")}>Annuler recherche</h5>}</div>
 
+      
       <ul className="countries-list">
-        {sortedData
+        {data
           .filter((country) => country.region.includes(selectedRadio))
-          .map((country) => (
-            <Card country={country} key={country.name.official} />
+          .sort((a, b) => b.area - a.area)
+          .filter((country, index) => index < rangeValue)
+          .map((country, index) => (
+            <Card country={country} key={`${country.name}-${index}`} />
           ))}
       </ul>
     </div>
