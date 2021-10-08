012b966d982ea525f01ac7c15540a0dca2ed45fd
Yoann Perez <yoann.perez@gmail.com>
2021-10-08 10:18:19 +0200

    Add admin privilèges to moderate forum

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import { React, useEffect, useState } from "react";
 import axios from "axios";
 import DeletePost from "./DeletePost";
 import authService from "../services/auth.service";
 import Comments from "./Comments";
+const API_URL = process.env.REACT_APP_API_ADRESS;
 require("dotenv").config();
 
 const Article = ({ article }) => {
   const user = authService.getCurrentUser();
   const [isEditing, setIsEditing] = useState(false);
   const [editedText, setEditedText] = useState("");
+  const [userData, setUserData] = useState([]);
 
   // -----------------    DATE PARSER    -----------------------
   const dateParser = (date) => {
@@ -24,6 +26,28 @@ const Article = ({ article }) => {
   };
   // END OF : ------------    DATE PARSER    --------------------
 
+  // -----------      Get User's Datas From API Function     ------------------
+  useEffect(() => {
+    if (user) {
+      const getUserData = () => {
+        axios.defaults.headers.common["Authorization"] = "Bearer " + user.token;
+        axios.get(API_URL + "/api/users/" + user.userId).then((res) => {
+          const imageUrl = res.data.user.imageUrl.replace("http://localhost:3000", API_URL);
+
+          setUserData({ id: res.data.user.id, username: res.data.user.username, email: res.data.user.email, isAdmin: res.data.user.isAdmin, description: res.data.user.description, imageUrl: imageUrl, createdAt: res.data.user.createdA, updatedAt: res.data.user.updatedAt });
+        });
+      };
+      getUserData();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  // -----------   END OF:    Get User's Datas From API Function   -------------
+
+
+
+
+
+
   // ---------------------    MODIFY POST LOGIC   ----------------
   const handleEdit = () => {
     const data = {
@@ -35,7 +59,7 @@ const Article = ({ article }) => {
   // END OF : ------------    MODIFY POST LOGIC   ----------------
 
   // ---------------------    CREATE DOM    ----------------------
-  if (article.UserId === user.userId) {
+  if (article.UserId === user.userId || (userData.isAdmin)) {
     return (
       <div className="article" style={{ background: isEditing ? "#f3feff" : "white", border: "2px solid white" }}>
         <div className="card-header">
--- src/components/CommentComp.js
+++ src/components/CommentComp.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import { React, useEffect, useState } from "react";
 import axios from "axios";
 import authService from "../services/auth.service";
 import DeleteComment from "./DeleteComment";
 require("dotenv").config();
 
 const CommentComp = (comment) => {
+  const API_URL = process.env.REACT_APP_API_ADRESS;
   const [isEditing, setIsEditing] = useState(false);
   const [editedText, setEditedText] = useState("");
+  const [userData, setUserData] = useState([]);
   const user = authService.getCurrentUser();
 
 
@@ -24,12 +26,34 @@ const CommentComp = (comment) => {
     return newDate;
   };
   // END OF : ------------    DATE PARSER    --------------------
+
+// -----------      Get User's Datas From API Function     ------------------
+useEffect(() => {
+  if (user) {
+    const getUserData = () => {
+      axios.defaults.headers.common["Authorization"] = "Bearer " + user.token;
+      axios.get(API_URL + "/api/users/" + user.userId).then((res) => {
+        const imageUrl = res.data.user.imageUrl.replace("http://localhost:3000", API_URL);
+
+        setUserData({ id: res.data.user.id, username: res.data.user.username, email: res.data.user.email, isAdmin: res.data.user.isAdmin, description: res.data.user.description, imageUrl: imageUrl, createdAt: res.data.user.createdA, updatedAt: res.data.user.updatedAt });
+      });
+    };
+    getUserData();
+  }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+}, []);
+// -----------   END OF:    Get User's Datas From API Function   -------------
+
+
+
+
+
+
   // ---------------------    MODIFY POST LOGIC   ----------------
   const handleEdit = () => {
     const data = {
       commentaire: editedText ? editedText : comment.comment.commentaire,
     };
-
     axios.put(process.env.REACT_APP_API_ADRESS + "/api/comments/" + comment.comment.id, data);
     setIsEditing(false);
   };
@@ -37,7 +61,7 @@ const CommentComp = (comment) => {
 
   // ---------------------    CREATE DOM    ----------------------
 
-  if (user.userId === comment.comment.UserId) {
+  if (user.userId === comment.comment.UserId || (userData.isAdmin)) {
     return (
       <div style={{ display: "flex", width: "100%" }}>
         {/* <p>commentComp</p> */}
