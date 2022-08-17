a4207f084ae3c8ce85b214dfdd04d54439039f4d
Yoann Perez <yoann.perez@gmail.com>
2022-08-17 18:19:22 +0200

    [UPDATE] Comments

---
+++ src/components/Comments/Comments.test.js
@@ -0,0 +1,8 @@
+import Comments from '.'
+import {render} from '@testing-library/react'
+
+describe("Comments", () => {
+    test('Should render without crash', async () => {
+        render(<Comments/>)
+    })
+})
